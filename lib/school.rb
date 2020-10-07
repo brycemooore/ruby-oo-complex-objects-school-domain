1d16aab1a8b951988a24616e1e3b102d23226c7f
Bryce Moore <brycemooore@gmail.com>
2020-10-07 18:18:14 -0400

    finished lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require 'pry'
+
+class School
+
+    attr_accessor :roster
+
+    def initialize(name)
+        @school_name = name
+        @roster = {}
+    end 
+
+    def add_student(name, grade)
+        self.roster[grade] ||= []
+        self.roster[grade] << name
+    end    
+
+    def grade(grade)
+        return self.roster[grade]
+    end
+
+    def sort
+        self.roster.each{|grade, students|
+            students.sort!
+        }
+        self.roster.sort_by{|grade, sudents| grade}.to_h
+    end
+
+end 
